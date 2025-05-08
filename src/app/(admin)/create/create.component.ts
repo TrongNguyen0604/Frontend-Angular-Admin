8351880ef43e09896448f3a90271e8660488b69a
Trong <trongnguye06042005@gmail.com>
2025-05-08 19:24:51 +0700

    Refactor product creation and listing components; update product data structure and enhance UI for better usability

--- src/app/(admin)/create/create.component.ts
+++ src/app/(admin)/create/create.component.ts
@@ -7,26 +7,41 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports: [CommonModule, FormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
   constructor(private api: HttpClient, private router: Router) {}
 
-  apiUrl: string = 'http://localhost:3000/projects';
+  apiUrl: string = 'http://localhost:3000/products';
 
-  onCreate(form: NgForm): void { 
+  onCreate(form: NgForm): void {
     if (form.invalid) {
       alert('Vui lòng nhập đúng thông tin!');
       return;
     }
 
-    this.api.post(this.apiUrl, form.value).subscribe(res => {
+    const formValue = form.value;
+
+    const product = {
+      name: formValue.name,
+      description: formValue.description,
+      category: formValue.category,
+      brand: formValue.brand,
+      price: +formValue.price,
+      images: [formValue.image1, formValue.image2, formValue.image3,formValue.image4].filter(Boolean),
+      status: formValue.status
+    };
+
+    this.api.post(this.apiUrl, product).subscribe(res => {
       if (res) {
-        alert('Thêm thành công');
-        this.router.navigate(['admin/list']);
+        alert('Thêm sản phẩm thành công!');
+        this.router.navigate(['/admin/list']);
       }
+    }, err => {
+      alert('Có lỗi xảy ra khi thêm sản phẩm.');
+      console.error(err);
     });
   }
 }
