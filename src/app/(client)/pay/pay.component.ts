0cdf892cd4424ffb79dd17087556ea9363d59a1e
TrongNguyen0604 <trongnguye06042005@gmail.com>
2025-06-12 21:39:08 +0700

    feat: Update payment component; add navigation to home after successful payment

--- src/app/(client)/pay/pay.component.ts
+++ src/app/(client)/pay/pay.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-
   selector: 'app-pay',
   imports: [
     CommonModule,
@@ -20,10 +20,11 @@ export class PayComponent implements OnInit {
   total = 0;
   cartItems: any[] = [];
 
+    constructor(private router: Router) {}
+
   ngOnInit(): void {
     const cart = localStorage.getItem('cart');
     this.cartItems = cart ? JSON.parse(cart) : [];
-
     this.total = this.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
   }
 
@@ -31,6 +32,7 @@ export class PayComponent implements OnInit {
     if (this.fullName && this.email && this.phone && this.address) {
       alert('Thanh toán thành công!');
       localStorage.removeItem('cart');
+      this.router.navigate(['/home']);
     } else {
       alert('Vui lòng điền đầy đủ thông tin.');
     }
